8906de03df7af318604c6020e988c5c0a26bd8b2
SimpleFreak <ISFreak@yandex.ru>
2024-10-03 15:18:24 +0500

    Added project

---
+++ ArithmeticLibrary/Arithmetic.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ArithmeticLibrary
+{
+    public class Arithmetic
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Деление на ноль недопустимо.");
+            }
+            return a / b;
+        }
+
+        public static double Power(double a, double b)
+        {
+            return Math.Pow(a, b);
+        }
+
+        public static double Modulo(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Деление на ноль недопустимо.");
+            }
+            return a % b;
+        }
+    }
+}
---
+++ DLL/Program.cs
@@ -0,0 +1,32 @@
+﻿using ArithmeticLibrary;
+using System;
+
+namespace DLL
+{
+    class Program
+    {
+        private static void Main()
+        {
+            double a = 10;
+            double b = 5;
+
+            double sum = Arithmetic.Add(a, b);
+            Console.WriteLine($"Сумма: {sum}");
+
+            double difference = Arithmetic.Subtract(a, b);
+            Console.WriteLine($"Разность: {difference}");
+
+            double product = Arithmetic.Multiply(a, b);
+            Console.WriteLine($"Произведение: {product}");
+
+            double quotient = Arithmetic.Divide(a, b);
+            Console.WriteLine($"Частное: {quotient}");
+
+            double power = Arithmetic.Power(a, b);
+            Console.WriteLine($"a в степени b: {power}");
+
+            double modulo = Arithmetic.Modulo(a, b);
+            Console.WriteLine($"Остаток от деления a на b: {modulo}");
+        }
+    }
+}
